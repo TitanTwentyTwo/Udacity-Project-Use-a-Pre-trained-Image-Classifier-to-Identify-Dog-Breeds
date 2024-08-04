d64636a9226038f296811a76b98607c56d0e1ed0
Sulagna Mondal <98563401+TitanTwentyTwo@users.noreply.github.com>
2024-08-05 03:51:36 +0530

    Add files via upload

---
+++ classify_images.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
+#                                                                             
+# PROGRAMMER: Sulagna Mondal
+# DATE CREATED: 10-07-2024                                
+# REVISED DATE: 16-07-2024
+# PURPOSE: Create a function classify_images that uses the classifier function 
+#          to create the classifier labels and then compares the classifier 
+#          labels to the pet image labels. This function inputs:
+#            -The Image Folder as image_dir within classify_images and function 
+#             and as in_arg.dir for function call within main. 
+#            -The results dictionary as results_dic within classify_images 
+#             function and results for the functin call within main.
+#            -The CNN model architecture as model within classify_images function
+#             and in_arg.arch for the function call within main. 
+#           This function uses the extend function to add items to the list 
+#           that's the 'value' of the results dictionary. You will be adding the
+#           classifier label as the item at index 1 of the list and the comparison 
+#           of the pet and classifier labels as the item at index 2 of the list.
+#
+##
+# Imports classifier function for using CNN to classify images 
+from classifier import classifier 
+
+# TODO 3: Define classify_images function below, specifically replace the None
+#       below by the function definition of the classify_images function. 
+#       Notice that this function doesn't return anything because the 
+#       results_dic dictionary that is passed into the function is a mutable 
+#       data type so no return is needed.
+# 
+# Imports classifier function for using CNN to classify images 
+
+
+def classify_images(images_dir, results_dic, model):
+    """
+    Creates classifier labels with classifier function, compares pet labels to 
+    the classifier labels, and adds the classifier label and the comparison of 
+    the labels to the results dictionary using the extend function.
+    
+    Parameters: 
+      images_dir - The (full) path to the folder of images that are to be
+                   classified by the classifier function (string)
+      results_dic - Results Dictionary with 'key' as image filename and 'value'
+                    as a List. Where the list will contain the following items: 
+                  index 0 = pet image label (string)
+                  NEW - index 1 = classifier label (string)
+                  NEW - index 2 = 1/0 (int)  where 1 = match between pet image
+                    and classifier labels and 0 = no match between labels
+      model - Indicates which CNN model architecture will be used by the 
+              classifier function to classify the pet images, values must be
+              either: resnet alexnet vgg (string)
+     Returns:
+           None - results_dic is mutable data type so no return needed.         
+    """
+    for key in results_dic:
+        # Full path to the image file
+        image_path = images_dir + key
+        
+        # Run the classifier function to get the classifier label
+        model_label = classifier(image_path, model)
+        
+        # Process the classifier label to be in lowercase and stripped of whitespace
+        model_label = model_label.lower().strip()
+        
+        # Check if the pet label matches the classifier label
+        truth = results_dic[key][0]
+        if truth in model_label:
+            results_dic[key].extend([model_label, 1])
+        else:
+            results_dic[key].extend([model_label, 0])
+
